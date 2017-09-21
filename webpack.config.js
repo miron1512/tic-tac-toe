89fa52d2d8d2269fd86203e1d0fc0de43dd5509c
Uladzislau Miranchuk <uladzislau_miranchuk@epam.com>
2017-09-22 02:25:42 +0300

    initialized react project with webpack, dev-server, hot-reload, redux, reselect and scss

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'app': [
+      'react-hot-loader/patch',
+      './src/index'
+    ]
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  devServer: {
+    contentBase: './build',
+    port: 3000,
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+
+      {
+        test: /\.scss$/,
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+    new ExtractTextWebpackPlugin({
+      filename: 'style.css',
+    }),
+  ]
+};
